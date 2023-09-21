17b3d0a59e59e6324b66a910ebf4e0aee18ea727
eternal <eternal.git@gmail.com>
2023-09-22 00:34:11 +0100

    Update header.tsx

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -15,40 +15,41 @@ function Header() {
 			<a className='flex items-center gap-[10px] mr-[10px] cursor-pointer hover:opacity-75 transition-opacity' href='/'>
 				<Logo width={64} height={64} />
 			</a>
-
-			<DropdownMenu>
-				<DropdownMenuTrigger>
-					<Button className='flex basis-auto shrink-0' variant='outline' size='icon'>
-						<Sun width={18} className='rotate-0 scale-100 transition-transform delay-100 dark:-rotate-90 dark:scale-0' />
-						<Moon height={18} className='absolute rotate-90 scale-0 transition-transform delay-100 dark:rotate-0 dark:scale-100' />
-						<span className='sr-only'>Toggle theme</span>
-					</Button>
-				</DropdownMenuTrigger>
-				<DropdownMenuContent>
-					<DropdownMenuLabel>
-						Appearance
-					</DropdownMenuLabel>
-					<DropdownMenuSeparator />
-					<DropdownMenuCheckboxItem
-						checked={theme === 'light'}
-						onClick={() => setTheme('light')}
-					>
-						Light
-					</DropdownMenuCheckboxItem>
-					<DropdownMenuCheckboxItem
-						checked={theme === 'dark'}
-						onClick={() => setTheme('dark')}
-					>
-						Dark
-					</DropdownMenuCheckboxItem>
-					<DropdownMenuCheckboxItem
-						checked={theme === 'system'}
-						onClick={() => setTheme('system')}
-					>
-						System
-					</DropdownMenuCheckboxItem>
-				</DropdownMenuContent>
-			</DropdownMenu>
+			<div className='ml-auto flex flex-row gap-3 items-center flex-shrink-0 flex-grow-0'>
+				<DropdownMenu>
+					<DropdownMenuTrigger>
+						<Button className='flex basis-auto shrink-0' variant='outline' size='icon'>
+							<Sun width={18} className='rotate-0 scale-100 transition-transform delay-100 dark:-rotate-90 dark:scale-0' />
+							<Moon height={18} className='absolute rotate-90 scale-0 transition-transform delay-100 dark:rotate-0 dark:scale-100' />
+							<span className='sr-only'>Toggle theme</span>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuLabel>
+							Appearance
+						</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuCheckboxItem
+							checked={theme === 'light'}
+							onClick={() => setTheme('light')}
+						>
+							Light
+						</DropdownMenuCheckboxItem>
+						<DropdownMenuCheckboxItem
+							checked={theme === 'dark'}
+							onClick={() => setTheme('dark')}
+						>
+							Dark
+						</DropdownMenuCheckboxItem>
+						<DropdownMenuCheckboxItem
+							checked={theme === 'system'}
+							onClick={() => setTheme('system')}
+						>
+							System
+						</DropdownMenuCheckboxItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
 		</div>
 	</nav>;
 }
