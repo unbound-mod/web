e0f44b6f6c3d114edef75fddebedd6d4f3fe82a0
Mario <me@marioparaschiv.com>
2024-08-03 20:59:23 +0100

    🐛 fix: font

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -7,8 +7,7 @@ export const path = '/';
 export function Page() {
 	return <div id='home-page' class='relative w-full mt-32 min-h-dvh'>
 		<div class='relative items-center gap-6 flex flex-col h-[100vh] w-full'>
-			{/* <Button variant='cta'>Begin your journey</Button> */}
-			<div class='mt-32 relative gap-4 md:gap-6 slide-in-from-bottom-36 zoom-in-90 animate-in duration-700 fade-in w-full items-center flex flex-col after:absolute after:z-10 after:bg-[linear-gradient(to_bottom,transparent_40%,hsl(var(--bg)/0.75)_60%,hsl(var(--bg)/1)_90%)] after:inset-0  after:pointer-events-none'>
+			<div class='mt-12 md:mt-32 relative gap-12 md:gap-6 slide-in-from-bottom-36 zoom-in-90 animate-in duration-700 fade-in w-full items-center flex flex-col after:absolute after:z-10 after:bg-[linear-gradient(to_bottom,transparent_40%,hsl(var(--bg)/0.75)_60%,hsl(var(--bg)/1)_90%)] after:inset-0  after:pointer-events-none'>
 				<div class='flex text-center items-center flex-col gap-2'>
 					<h1 class='text-5xl lg:text-6xl text-center font-secondary font-bold bg-gradient-to-br from-primary-fg to-secondary-fg bg-clip-text text-transparent z-10'>
 						Unbound
